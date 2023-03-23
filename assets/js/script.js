a964ec3a9fc1f4d14f070db62dde21c973138f68
alexberg90 <ayazya@code.edu.az>
2023-03-23 17:34:24 +0400

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,24 @@
 $(document).ready(function() {
     $('form').submit(function(e) {
       e.preventDefault();
-      var city = $('#city').val();
-      var day = $('#day').val();
-      var month = $('#month').val();
+      let city=$('#city').val();
+      let day = $('#day').val();
+      let month = $('#month').val();
       
       $.ajax({
         url: 'http://api.aladhan.com/v1/calendar/2023/' + month + '?latitude=40.1431&longitude=47.5769&method=2',
         type: 'GET',
-        dataType: 'json',
         success: function(data) {
           var timings = data.data[day-1].timings;
-          $('.sag').eq(0).text(timings.Imsak);
-          $('.sag').eq(1).text(timings.Sunrise);
-          $('.sag').eq(2).text(timings.Dhuhr);
-          $('.sag').eq(3).text(timings.Asr);
-          $('.sag').eq(4).text(timings.Maghrib);
-          $('.sag').eq(5).text(timings.Isha);
-          $('.sag').eq(6).text(timings.Midnight);
+          $('.time').eq(0).text(timings.Imsak);
+          $('.time').eq(1).text(timings.Sunrise);
+          $('.time').eq(2).text(timings.Dhuhr);
+          $('.time').eq(3).text(timings.Asr);
+          $('.time').eq(4).text(timings.Maghrib);
+          $('.time').eq(5).text(timings.Isha);
+          $('.time').eq(6).text(timings.Midnight);
         },
-        error: function(jqXHR, textStatus, errorThrown) {
-          console.log(textStatus + ': ' + errorThrown);
-        }
+        
       });
     });
   });
