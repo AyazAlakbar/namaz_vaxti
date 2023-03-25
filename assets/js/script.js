0cbd68f780190f9bc06c7ad818a5c0172b01dc8b
alexberg90 <ayazya@code.edu.az>
2023-03-25 12:49:06 +0400

    new update

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
       url: 'http://api.aladhan.com/v1/calendar/2023/' + month + '?latitude=40.1431&longitude=47.5769&method=2',
       type: 'GET',
       success: function (data) {
-        var timings = data.data[day - 1].timings;
+        let timings = data.data[day - 1].timings;
         $('.time').eq(0).text(timings.Imsak);
         $('.time').eq(1).text(timings.Sunrise);
         $('.time').eq(2).text(timings.Dhuhr);
